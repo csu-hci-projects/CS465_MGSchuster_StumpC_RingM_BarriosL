dc631e3b3affd1411d43755392492aec24e6b177
Maggie Ring <77123036+MargaretRing@users.noreply.github.com>
2025-04-17 09:43:42 -0600

    haiiii :333

---
+++ LockPicking_Project/Assets/ButtonFollowVisual.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ButtonFollowVisual : MonoBehaviour
+{
+    public Transform visualTarget;
+    public Vector3 localAxis;
+
+    private Vector3 offset;
+    private Transform pokeAttachTransform;
+
+    private XRBaseInteractable interactable;
+    private bool isFollowing = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactable = GetComponent<XRBaseInteractable>();
+        interactable.hoverEntered.AddListener(Follow);
+        interactable.hoverExited.AddListener(Reset);
+    }
+
+    public void Follow(BaseInteractionEventArgs hover)
+    {
+        if (hover.interactorObject is XRPokeInteractor)
+        {
+            XRPokeInteractor interactor = (XRPokeInteractor)hover.interactorObject;
+            isFollowing = true;
+            pokeAttachTransform = interactor.attachTransform;
+            offset = visualTarget.position - pokeAttachTransform.position;
+        }
+    }
+
+    public void Reset(BaseInteractionEventArgs hover)
+    {
+        if (hover.interactorObject is XRPokeInteractor)
+        {
+            isFollowing = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFollowing)
+        {
+            Vector3 localTargetPosition = visualTarget.InverseTransformPoint(pokeAttachTransform.position + offset);
+            Vector3 contrainedLocalTargetPosition = Vector3.Project(localTargetPosition, localAxis);
+
+            visualTarget.position = visualTarget.TransformPoint(contrainedLocalTargetPosition);
+        }
+
+    }
+}
