796e0fcae2dd95f847f87551a78f8aa70d0ab1f0
Mallory Guerdet-Schuster <guerschu@colostate.edu>
2025-04-21 13:36:06 -0600

    Fixed timer

--- LockPicking_Project/Assets/lock-model/Scripts/Timer.cs
+++ LockPicking_Project/Assets/lock-model/Scripts/Timer.cs
@@ -4,7 +4,6 @@ using System.IO;
 
 public class Timer : MonoBehaviour
 {
-    public Text timertex;  // Legacy UI Text component
     public Text lapText;
 
     private float timeElapsed = 0f;
@@ -15,14 +14,16 @@ public class Timer : MonoBehaviour
 
     void Start()
     {
-        timertex.text = "0.00";
-
         filePath = Path.Combine(Application.persistentDataPath, "LapTimes.csv");
 
         if (!File.Exists(filePath))
         {
             File.WriteAllText(filePath, "Lap,Time (seconds)\n");
         }
+
+        timeElapsed = 0f;
+        isTiming = true;
+        Debug.Log("Timer started automatically when scene loaded!");
     }
 
     void Update()
@@ -30,19 +31,9 @@ public class Timer : MonoBehaviour
         if (isTiming)
         {
             timeElapsed += Time.deltaTime;
-            timertex.text = timeElapsed.ToString("F2");
         }
     }
 
-    // Start button calls this
-    public void StartTimer()
-    {
-        timeElapsed = 0f;
-        isTiming = true;
-        Debug.Log("Timer started!");
-    }
-
-    // Stop button calls this
     public void StopTimer()
     {
         if (!isTiming) return;
